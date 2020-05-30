19a71dcfec1f68e9d1a8a8ecd575314e54a05d76
Ray Ros <ray.ros404@gmail.com>
2020-05-29 19:42:13 -0700

    created conflict in master

--- siete-y-medio.cpp
+++ siete-y-medio.cpp
@@ -7,7 +7,10 @@
 #include "cards.h"
 using namespace std;
 
+cout << "Here I am adding an additional line to create a conflict when I merge with the conflict branch.";
 cout << "Vim test. Practicing to use vim.";
+cout << "Hopefully, but creating additional cout statements, git will not know how to merge these two.";
+cout << "Pic10c is pretty fun.";
 
 // Global constants (if any)
 
